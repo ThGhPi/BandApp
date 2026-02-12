1c83597029597302dfcaefea3ca35a848df24ce4
ThGhPi <thomas.gheerbrant@live.fr>
2026-02-12 16:23:13 +0100

    création angular app et survey feature (début)

---
+++ banda-front/src/app/features/survey/pages/survey-list/survey-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-list',
+  imports: [],
+  templateUrl: './survey-list.html',
+  styleUrl: './survey-list.css',
+})
+export class SurveyList {
+
+}
